323afa0c4d739a6d0d5d6f55246ffeeaa1b82621
Emir <emirhansirkeci8@gmail.com>
2024-02-16 21:26:28 +0300

    Add tab listener to catch url changes

--- content/netflix/index.js
+++ content/netflix/index.js
@@ -1,29 +1,43 @@
-const targetElement = document.querySelector(
-  "#appMountPoint > div > div > div.watch-video"
-);
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.message === "tab-updated") {
+    const url = document.location.href;
 
-console.log("Skip it");
-
-const observer = new MutationObserver((mutations) => {
-  mutations.forEach((mutation) => {
-    const node = mutation.addedNodes[0];
-    if (!node) return;
-
-    const skipButton = node.querySelector(".watch-video--skip-content-button");
-
-    if (skipButton) {
-      return skipButton.click();
+    if (url.includes("/watch")) {
+      main();
     }
-
-    const nextEpisodeButton = node.querySelector(
-      "button[data-uia='next-episode-seamless-button-draining']"
-    );
-
-    if (nextEpisodeButton) {
-      return nextEpisodeButton.click();
-    }
-  });
+  }
 });
 
-const config = { childList: true, subtree: true };
-observer.observe(targetElement, config);
+function main() {
+  const targetElement = document.querySelector(
+    "#appMountPoint > div > div > div.watch-video"
+  );
+
+  console.log("Skip it");
+
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      const node = mutation.addedNodes[0];
+      if (!node) return;
+
+      const skipButton = node.querySelector(
+        ".watch-video--skip-content-button"
+      );
+
+      if (skipButton) {
+        return skipButton.click();
+      }
+
+      const nextEpisodeButton = node.querySelector(
+        "button[data-uia='next-episode-seamless-button-draining']"
+      );
+
+      if (nextEpisodeButton) {
+        return nextEpisodeButton.click();
+      }
+    });
+  });
+
+  const config = { childList: true, subtree: true };
+  observer.observe(targetElement, config);
+}
