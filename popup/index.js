b65736c73eed4a37148bdd18e157d9a6f693557f
Emir <emirhansirkeci8@gmail.com>
2024-02-22 23:42:42 +0300

    Store and retrieve switch states in Chrome local storage

---
+++ popup/index.js
@@ -0,0 +1,33 @@
+let netflix = document.querySelector("#youtube");
+let youtube = document.querySelector("#netflix");
+
+(async function updateSwitches() {
+  const { switchStates } = await chrome.storage.local.get(["switchStates"]);
+
+  if (!switchStates) {
+    youtube.checked = true;
+    netflix.checked = true;
+    return;
+  }
+
+  youtube.checked = switchStates.youtube;
+  netflix.checked = switchStates.netflix;
+})();
+
+youtube.addEventListener("click", () => {
+  chrome.storage.local.set({
+    switchStates: {
+      youtube: youtube.checked,
+      netflix: netflix.checked,
+    },
+  });
+});
+
+netflix.addEventListener("click", async () => {
+  chrome.storage.local.set({
+    switchStates: {
+      youtube: youtube.checked,
+      netflix: netflix.checked,
+    },
+  });
+});
